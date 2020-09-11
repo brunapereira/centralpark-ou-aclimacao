1eef89202256e5d78d1c08b2539283c44b1c3d6d
Anike Arni <anikarni@gmail.com>
2020-09-11 12:09:23 +0200

    Fix firebase build

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+	plugins: [
+		{
+			resolve: "gatsby-plugin-firebase",
+			options: {
+				credentials: {
+					apiKey: "AIzaSyBaEnpAJS4j3nAmhirc-mAptMATy-5wUy8",
+					authDomain: "mitonaro-f6796.firebaseapp.com",
+					databaseURL: "https://mitonaro-f6796.firebaseio.com",
+					projectId: "mitonaro-f6796",
+					storageBucket: "mitonaro-f6796.appspot.com",
+					messagingSenderId: "247161612247",
+					appId: "1:247161612247:web:02bd008c034d69d11b0d92",
+					measurementId: "G-VV2ZHD36RM"
+				}
+			}
+		}
+	],
 }
