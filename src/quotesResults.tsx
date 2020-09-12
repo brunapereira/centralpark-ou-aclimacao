68b09f6e1d285e3093db860e92eb5fe00786a8d0
Anike Arni <anikarni@gmail.com>
2020-09-12 19:52:36 +0200

    Mostra mensagem se não houver resultados

--- src/quotesResults.tsx
+++ src/quotesResults.tsx
@@ -8,14 +8,30 @@ interface QuoteRecord {
   source: string;
 }
 
+function QuotesList(props) {
+  const { quotes } = props
+  return (
+    <div>
+      {quotes.map((q, i) => <p key={i}>{q.value}</p>)}
+      {!quotes.length ? <span>Sua busca não teve resultados</span> : ''}
+    </div>
+  )
+}
+
 export default function QuotesResults(props) {
   const [snapshot, isLoading, error] = useListVals<QuoteRecord>(firebase.database().ref("/quotes"))
   return (
     <div>
       {error && <strong>Erro: {error}</strong>}
       {isLoading && <span>Buscando...</span>}
-      {(!isLoading && snapshot) &&
-          snapshot.filter(q => q.value.includes(props.searchTerms) || q.labels.find(l => l.includes(props.searchTerms))).map((q, i) => <p key={i}>{q.value}</p>)}
+      {
+        (!isLoading && snapshot) &&
+          <QuotesList quotes={
+            snapshot.filter(q => 
+              q.value.includes(props.searchTerms) || 
+              q.labels.find(l => l.includes(props.searchTerms)))
+            } />
+      }
     </div>
   )
 }
