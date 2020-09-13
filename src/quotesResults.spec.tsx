210e1f244421895b39177663bf06a3e36250fba1
Anike Arni <anikarni@gmail.com>
2020-09-13 09:55:21 +0200

    Testa renderizacao de frases

--- src/quotesResults.spec.tsx
+++ src/quotesResults.spec.tsx
@@ -1,16 +1,51 @@
 import React from "react"
-import {shallow} from 'enzyme'
+import {shallow, mount} from 'enzyme'
 
 describe("QuotesResults", () => {
+  const mockUseListVals = jest.fn()
+  
   beforeEach(() => {
-    const mockRef = jest.fn().mockReturnValue({ snapshots: { val: jest.fn() } })
-    const mockDatabase = jest.fn().mockReturnValue({ ref: mockRef })
+    const mockDatabase = jest.fn().mockReturnValue({ ref: jest.fn() })
     jest.mock('gatsby-plugin-firebase', () => ({ database: mockDatabase }))
+    jest.mock('react-firebase-hooks/database', () => ({ useListVals: mockUseListVals }))
   })
 
   it("renders a loading message", () => {
+    mockUseListVals.mockReturnValue([false, true, false])
     const QuotesResults = require('./quotesResults').default
     const component = shallow(<QuotesResults />)
     expect(component.text()).toContain("Buscando")
+  })
+
+  it("renders filtered list by sentence", () => {
+    mockUseListVals.mockReturnValue([[
+      {value: 'Primeira frase', labels: ['familia']},
+      {value: 'Segunda frase', labels: ['familia']},
+    ], false, false])
+
+    const QuotesResults = require('./quotesResults').default
+    const component = mount(<QuotesResults searchTerms="Primeira" />)
+    expect(component.text()).toContain("Primeira frase")
+    expect(component.text()).not.toContain("Segunda frase")
+  })
+
+  it("renders filtered list by labels", () => {
+    mockUseListVals.mockReturnValue([[
+      {value: 'Primeira frase', labels: ['outro']},
+      {value: 'Segunda frase', labels: ['familia']},
+    ], false, false])
+
+    const QuotesResults = require('./quotesResults').default
+    const component = mount(<QuotesResults searchTerms="outro" />)
+    expect(component.text()).toContain("Primeira frase")
+    expect(component.text()).not.toContain("Segunda frase")
+  })
+
+  it("renders error", () => {
+    mockUseListVals.mockReturnValue([false, false, "Some error"])
+
+    const QuotesResults = require('./quotesResults').default
+    const component = mount(<QuotesResults searchTerms="outro" />)
+    expect(component.text()).toContain("Erro: Some error")
   })
 })
