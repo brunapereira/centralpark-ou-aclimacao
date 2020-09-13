53f2cd385e30b06b2bdf77a175c9cc4ac25a04e5
Anike Arni <anikarni@gmail.com>
2020-09-13 10:22:16 +0200

    Substitui busca por biblioteca

--- src/quotesResults.tsx
+++ src/quotesResults.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import firebase from "gatsby-plugin-firebase"
 import { useListVals } from "react-firebase-hooks/database"
+import Fuse from 'fuse.js'
 
 interface QuoteRecord {
   value: string;
@@ -10,9 +11,13 @@ interface QuoteRecord {
 
 function QuotesList(props) {
   const { quotes } = props
+
   return (
     <div>
-      {quotes.map((q, i) => <p key={i}>{q.value}</p>)}
+      {quotes
+         .filter(q => q.score < 0.6)
+         .sort((a, b) => a.score - b.score)
+         .map((q, i) => <p key={i}>{q.item.value}</p>)}
       {!quotes.length ? <span>Sua busca não teve resultados</span> : ''}
     </div>
   )
@@ -20,6 +25,12 @@ function QuotesList(props) {
 
 export default function QuotesResults(props) {
   const [snapshot, isLoading, error] = useListVals<QuoteRecord>(firebase.database().ref("/quotes"))
+  const searchOptions = {
+    includeScore: true, 
+    ignoreLocation: true,
+    keys: ['value', { name: 'labels', weight: 0.3 }]
+  }
+
   return (
     <div>
       {error && <strong>Erro: {error}</strong>}
@@ -27,10 +38,7 @@ export default function QuotesResults(props) {
       {
         (!isLoading && snapshot) &&
           <QuotesList quotes={
-            snapshot.filter(q => 
-              q.value.includes(props.searchTerms) || 
-              q.labels.find(l => l.includes(props.searchTerms)))
-            } />
+            new Fuse(snapshot, searchOptions).search(props.searchTerms) } />
       }
     </div>
   )
