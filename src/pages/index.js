519a84ed35bd3be135c80708e78704083023051a
Bruna Pereira <ppbruna2@gmail.com>
2021-03-03 15:50:46 -0300

    create home styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import SEO from '../seo'
+import home from '../../static/home.jpg'
 
 export default function Home() {
   return (
     <div id="root">
       <SEO title="Início" />
-      <div id="titleSection"></div>
+      <div id="main">
+        <div className="title">
+          <h2>Central Park <br/> ou Aclimação?</h2>
+          <p>O Quiz!</p>
+        </div>
+        <button type="submit" className="startButton">Começar</button>
+      </div>
     </div>
   )
 }
